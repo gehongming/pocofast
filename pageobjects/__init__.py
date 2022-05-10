971aa1526c7732698559741829db7ce6846a5f1b
葛红明 <gehm@ti-net.com.cn>
2022-05-10 15:00:05 +0800

    first commit

---
+++ pageobjects/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2022/5/2 16:22
+@Auth ： ghm
+@File ：__init__.py.py
+@IDE ：PyCharm
+@Motto：ABC(Always Be Coding)
+
+"""
---
+++ pageobjects/basepage.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2022/5/5 22:05
+@Auth ： ghm
+@File ：basepage.py
+@IDE ：PyCharm
+@Motto：ABC(Always Be Coding)
+
+"""
